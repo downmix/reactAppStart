860c38f8719ccb3169dab0beebc3389e0ae5cf99
DESKTOP-J94MAUB\downmix <downmix@downmix.net>
2017-05-03 23:23:08 +0900

    hello world

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './Components/Logo';
+
+ReactDOM.render(
+  <h1>
+    <Logo />Welcome to the APP!
+  </h1>
+  ,
+  document.getElementById('app')
+);
